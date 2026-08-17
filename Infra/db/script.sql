8dc9bcbc33f559b4eae7dd4d70bbde1cd020c81e
davi_sehnem <davi_sehnem@estudante.sesisenai.org.br>
2026-08-17 08:34:48 -0300

    feat - código sql

--- Infra/db/script.sql
+++ Infra/db/script.sql
@@ -2,12 +2,20 @@ CREATE DATABASE IF NOT EXISTS sistema_cadastro_pratos;
 
 USE sistema_cadastro_pratos;
 
-
-CREATE TABLE usuario(
+CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255)NOT NULL,
-    senha VARCHAR(255)NOT NULL
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pratos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    categoria VARCHAR(255) NOT NULL,
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
 
-INSERT INTO usuario (usuario,senha) VALUE ('admin','123');
